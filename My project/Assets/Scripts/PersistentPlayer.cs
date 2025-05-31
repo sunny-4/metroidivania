4534b8864ea7ab9911df8df1087d19d65b46a88d
Satyakam-Mishra <mishra.satyakam03@gmail.com>
2025-05-31 16:43:23 +0530

    Completed room changer
    Some bugs remain

---
+++ My project/Assets/Scripts/PersistentPlayer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PersistentPlayer : MonoBehaviour
+{
+    private static PersistentPlayer instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
