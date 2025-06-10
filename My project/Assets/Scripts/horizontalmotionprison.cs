bc69f8e6edd2d56b3e45c7e7a9941cdd4c9784c1
shreyaanbu <shreyaanbu@gmail.com>
2025-06-10 11:53:55 +0530

    intermediate
    solving merge conflict in prefab file

--- My project/Assets/Scripts/horizontalmotionprison.cs
+++ My project/Assets/Scripts/horizontalmotionprison.cs
@@ -27,4 +27,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
             velocity *= -1;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
 }
