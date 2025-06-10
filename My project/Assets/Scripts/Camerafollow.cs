0b9077ec77361585311de82fc5f38f9b176a84ae
shreyaanbu <shreyaanbu@gmail.com>
2025-06-10 11:26:01 +0530

    Revert "Merge pull request #31 from sunny-4/shreya"
    This reverts commit d5b83f7eb295b94f412be510baac8b5325757560, reversing
    changes made to 1a85c3bad5a1a73aa15d464009f36639fd180dbb.

--- My project/Assets/Scripts/Camerafollow.cs
+++ My project/Assets/Scripts/Camerafollow.cs
@@ -1,31 +1,38 @@
+
 using UnityEngine;
 
 public class Camerafollow : MonoBehaviour
 {
-    private Transform player;
+    private Transform Player;// Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform temppos;
 
-    [SerializeField] private float minX = -100.0f;
+    [SerializeField] private float minX = -100.0f;// Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float maxX = 100.0f;
     [SerializeField] private float minY = -100.0f;
     [SerializeField] private float maxY = 100.0f;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject playerObj = GameObject.FindWithTag("Player");
-        if (playerObj != null)
-        {
-            player = playerObj.transform;
-        }
+        Player = GameObject.FindWithTag("Player").transform;
     }
 
+    // Update is called once per frame
     void LateUpdate()
     {
-        if (!player) return;
+        if (!Player)
+        {
+            return;
+        }
+        Transform temppos = GameObject.FindWithTag("MainCamera").transform;
+        Vector3 camPos = new Vector3(Player.position.x, Player.position.y, -10f);
 
-        Vector3 camPos = new Vector3(player.position.x, player.position.y, -10f);
+        // Clamp the camera position
         camPos.x = Mathf.Clamp(camPos.x, minX, maxX);
         camPos.y = Mathf.Clamp(camPos.y, minY, maxY);
 
-        transform.position = camPos; // Don't re-fetch camera each frame
+        // Apply the clamped position to the camera
+        temppos.position = camPos;
+
+
     }
 }
