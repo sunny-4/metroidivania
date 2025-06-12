3256ded6f620eff7b996a87fcbedb51f6891d133
Satyakam-Mishra <mishra.satyakam03@gmail.com>
2025-06-12 20:07:23 +0530

    Added introducer

---
+++ My project/Assets/Scripts/Roomchangerintro.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class Roomchangerintro : MonoBehaviour
+{
+    [SerializeField] private bool alpha;
+    private GameObject playerObject;
+    void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+        if (alpha)
+        {
+            if (Keyboard.current[Key.B].wasPressedThisFrame)
+            {
+                SceneManager.LoadScene("Store Room");
+            }
+        }
+
+        else
+        {
+            if ((playerObject.transform.position.x < -1) && (playerObject.transform.position.x > -6) && Keyboard.current[Key.E].wasPressedThisFrame)
+            {
+                SceneManager.LoadScene("Introduction");
+            }
+        }
+    }
+}
